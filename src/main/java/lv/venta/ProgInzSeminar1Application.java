6927792068e695da375736f64ba88e60b55369c2
Ilja Avlass <epastsneeksiste@gmail.com>
2024-04-19 12:17:58 +0300

    change

--- src/main/java/lv/venta/ProgInzSeminar1Application.java
+++ src/main/java/lv/venta/ProgInzSeminar1Application.java
@@ -31,23 +31,24 @@ public class ProgInzSeminar1Application {
 				//izsaukt update cour repo
 				
 				
-				Product p11 = new Product("productTen", 0.99f, "aaaa", 3);
-				Product p22 = new Product("productEleven", 1.99f, "bbb", 2);
-				Product p33 = new Product("productTwelve", 2.99f, "cccc", 1);
+				//Product p11 = new Product("productTen", 0.99f, "aaaa", 3);
+				//Product p22 = new Product("productEleven", 1.99f, "bbb", 2);
+				//Product p33 = new Product("productTwelve", 2.99f, "cccc", 1);
 				
-				productRepo.save(p11);
-				productRepo.save(p22);
-				productRepo.save(p33);
+				//productRepo.save(p11);
+				//productRepo.save(p22);
+				//productRepo.save(p33);
 				
-				System.out.println("how many product " + productRepo.count());
-				System.out.println("get product by id (102): " + productRepo.findById(3).get());
-				/*
-				Product productForDeliting = productRepo.findById(2).get();
-				productRepo.delete(productForDeliting);
-				Product productForUpdate = productRepo.findById(3).get();
+				//System.out.println("how many product " + productRepo.count());
+				//System.out.println("get product by id (504): " + productRepo.findById(504).get());
+				
+				//Product productForDeliting = productRepo.findById(504).get();
+				//productRepo.delete(productForDeliting);
+				
+				Product productForUpdate = productRepo.findById(503).get();
 				productForUpdate.setPrice(5.99f);
 				productRepo.save(productForUpdate);
-				*/
+				
 				
 			}
 		};
