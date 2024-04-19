4ea617e8c912abbe4b48b68948e5ef8327424adf
Ilja Avlass <epastsneeksiste@gmail.com>
2024-04-19 10:33:16 +0300

    TRY 2 add more code

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,14 @@
 package lv.venta.model;
 
+import org.hibernate.annotations.Table;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -15,17 +24,37 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 //@AllArgsConstructor
+@jakarta.persistence.Table(name = "ProductTable")
+@Entity
 public class Product {
 	
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@NotNull
 	@Size(min = 3, max = 50)
 	@Pattern(regexp = "[A-Za-z ]+")
+	@Column(name = "Title")
 	private String title;
+	
+	@Max(1000)
+	@Min(0)
+	@Column(name = "Price")
 	private float price;
+	
+	
+	@NotNull
+	@Size(min = 3, max = 100)
+	@Pattern(regexp = "[A-Za-z ]+")
+	@Column(name = "Description")
 	private String description;
+	
+	@Max(500)
+	@Min(0)
+	@Column(name = "Quantity")
 	private int quantity;
 	
 	private static int counter = 0;
