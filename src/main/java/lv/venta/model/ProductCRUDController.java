cae8723d54626a0549289a800f4c588eb53b2fd7
Ilja Avlass <epastsneeksiste@gmail.com>
2024-04-26 12:13:33 +0300

    controller

--- src/main/java/lv/venta/model/ProductCRUDController.java
+++ src/main/java/lv/venta/model/ProductCRUDController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.service.IProductCRUDService;
@@ -31,7 +33,7 @@ public class ProductCRUDController {
 		
 	}
 	
-	@GetMapping("/one") //localhost:8080/product/crud/one?id=2
+	@GetMapping("/one") //localhost:8080/product/crud/one?id=
 	public String getProductCRUDone(@RequestParam("id")int id, Model model) {
 		try {
 			Product foundProduct = crudService.retriveById(id);
@@ -43,9 +45,29 @@ public class ProductCRUDController {
 		}
 	}
 	
+	@GetMapping("/all/{id}") //localhost:8080/product/crud/all/{id}
+	public String getProductCRUDall(@PathVariable("id") int id, Model model) {
+		try {
+			Product foundProduct = crudService.retriveById(id);
+			model.addAttribute("mydata", foundProduct);
+			return "more-product-in-page";
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+	}
 	
+	@GetMapping("/insert") // localhost:8080/product/crud/insert
+	public String getProductCrudInsert(Model model) {
+		model.addAttribute("product", new Product());
+		return "product-insert-page";
+	}
 	
-	
+	@PostMapping("/insert")
+	public String  postProductCrudInsert(Product product) {
+		System.out.println(product);
+		return "redirect:/product/crud/all";
+	}
 	
 	
 }
